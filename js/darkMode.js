6b56f6621669d982d9664d5c2763bc50657e0e77
Julian Gareca Kandrachoff <jugakan@gmail.com>
2021-08-11 00:12:01 -0300

    dark mode

--- js/darkMode.js
+++ js/darkMode.js
@@ -1,9 +1,9 @@
-
 const cambio = () => {
-    if(localStorage.getItem("modo") == "oscuro"){
-        aclarar()
-    } else{
+    if (localStorage.getItem("modo") == "claro") {
         oscurecer();
+
+    } else {
+        aclarar();
     }
 }
 
@@ -19,8 +19,8 @@ const oscurecer = () => {
     <path d="M2 6a6 6 0 1 1 10.174 4.31c-.203.196-.359.4-.453.619l-.762 1.769A.5.5 0 0 1 10.5 13h-5a.5.5 0 0 1-.46-.302l-.761-1.77a1.964 1.964 0 0 0-.453-.618A5.984 5.984 0 0 1 2 6zm3 8.5a.5.5 0 0 1 .5-.5h5a.5.5 0 0 1 0 1l-.224.447a1 1 0 0 1-.894.553H6.618a1 1 0 0 1-.894-.553L5.5 15a.5.5 0 0 1-.5-.5z"/>
   </svg>`
 
-  localStorage.setItem("modo","oscuro")
-       
+    localStorage.setItem("modo", "oscuro")
+
 }
 
 const aclarar = () => {
@@ -34,9 +34,14 @@ const aclarar = () => {
     <path d="M2 6a6 6 0 1 1 10.174 4.31c-.203.196-.359.4-.453.619l-.762 1.769A.5.5 0 0 1 10.5 13a.5.5 0 0 1 0 1 .5.5 0 0 1 0 1l-.224.447a1 1 0 0 1-.894.553H6.618a1 1 0 0 1-.894-.553L5.5 15a.5.5 0 0 1 0-1 .5.5 0 0 1 0-1 .5.5 0 0 1-.46-.302l-.761-1.77a1.964 1.964 0 0 0-.453-.618A5.984 5.984 0 0 1 2 6zm6-5a5 5 0 0 0-3.479 8.592c.263.254.514.564.676.941L5.83 12h4.342l.632-1.467c.162-.377.413-.687.676-.941A5 5 0 0 0 8 1z"/>
   </svg>`
 
-  localStorage.setItem("modo","claro")
-       
+    localStorage.setItem("modo", "claro")
+
 }
 
-
 $("#dark").click(cambio)
+
+if (localStorage.getItem("modo") == "oscuro") {
+    oscurecer();
+} else {
+    aclarar()
+}
